3494b7e1461eabf05537b0cacfe2bda15c97623d
zahra rezaei <rezaeizahra1402@gmail.com>
2023-07-26 17:52:44 +0430

    add cbv for homepage

--- core/home/urls.py
+++ core/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from . import views
 
+app_name = 'home'
 urlpatterns = [
-    path('', home, name='home' )
+    path('', views.HomeView.as_view(), name='home')
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
-
-
-def home(request):
-    return render(request, 'home/index.html')
+class HomeView(View):
+    def get (self, request):
+        return render(request, 'home/index.html')
