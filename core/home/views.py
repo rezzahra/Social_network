082ebca13cf65f2a7d2d1c9f5111d7722aacb398
zahra rezaei <rezaeizahra1402@gmail.com>
2023-07-26 17:57:46 +0430

    update homepage by genericView

--- core/home/views.py
+++ core/home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views import View
+from django.views import View, generic
 
 
 # Create your views here.
-class HomeView(View):
-    def get (self, request):
-        return render(request, 'home/index.html')
+
+# class HomeView(View):
+#     def get (self, request):
+#         return render(request, 'home/index.html')
+class HomeView(generic.TemplateView):
+    template_name = 'home/index.html'
